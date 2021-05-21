4238ea4cdd3353e8beb82d2a896fd3a5fa0b0f37
Evgeni Konstantinov <evgeni.k@burns.app>
2021-05-21 10:14:00 +0300

    update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,5 @@
+import MultiSelect from './components/multi-select/multi-select';
 import Table from './components/table/table';
-new Table();
+
+customElements.define('auction-table', Table);
+customElements.define('multi-select', MultiSelect);
