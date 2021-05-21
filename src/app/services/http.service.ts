c800491a5170095a301f9b04d95a7e6047019bd1
Evgeni Konstantinov <evgeni.k@burns.app>
2021-05-21 10:13:10 +0300

    create http service

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,10 @@
+export class HttpService {
+  public static api<T>(url: string): Promise<T> {
+    return fetch(url).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
+}
